b029c94f17d7808b39fa5cd9a91e611581bdf8d3
Fernando Weber <101434984+FernandoRhenan@users.noreply.github.com>
2026-04-08 00:39:21 +0000

    adds `/api/v1/migrations` endpoint

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,38 @@
+import migrationRunner from 'node-pg-migrate'
+import database from 'infra/database.js'
+import { join } from 'node:path'
+
+export default async function migrations(request, response) {
+
+  const dbClient = await database.getNewClient()
+
+  const defaultOptions = {
+    dbClient: dbClient,
+    dir: join('infra', 'migrations'),
+    direction: 'up',
+    verbose: true,
+    migrationsTable: 'pgmigrations',
+    dryRun: true
+  }
+
+  if (request.method === 'GET') {
+    const pendingMigrations = await migrationRunner(defaultOptions)
+    await dbClient.end()
+    return response.status(200).json(pendingMigrations)
+  }
+
+  if (request.method === 'POST') {
+    const migratedMigrations = await migrationRunner({ ...defaultOptions, dryRun: false })
+
+    await dbClient.end()
+
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations)
+    } else {
+      return response.status(200).json(migratedMigrations)
+    }
+  }
+
+  return response.status(405)
+
+}
