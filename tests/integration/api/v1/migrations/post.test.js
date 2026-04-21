2ea6df6ef9024fc6db4b37e641f555e32a731171
FernandoRhenan <fernandorhenan9@gmail.com>
2026-04-21 10:39:04 -0300

    test(bdd): change the `tests` descriptions

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,28 +6,42 @@ beforeAll(async () => {
   await database.query('drop schema public cascade; create schema public;')
 })
 
-test('POST to /api/v1/status should return 200', async () => {
-  const response = await fetch('http://localhost:3000/api/v1/migrations', {
-    method: 'POST',
+describe('POST /api/v1/migrations', () => {
+  describe('Anonymous user', () => {
+    describe('Running pending migrations', () => {
+      test('For the first time', async () => {
+        const response = await fetch(
+          'http://localhost:3000/api/v1/migrations',
+          {
+            method: 'POST',
+          }
+        )
+
+        expect(response.status).toBe(201)
+
+        const responseBody = await response.json()
+
+        expect(Array.isArray(responseBody)).toBe(true)
+        expect(responseBody.length).toBeGreaterThan(0)
+        expect(responseBody[0]).not.toBeUndefined()
+      })
+
+      test('For the second time', async () => {
+        const response = await fetch(
+          'http://localhost:3000/api/v1/migrations',
+          {
+            method: 'POST',
+          }
+        )
+
+        expect(response.status).toBe(200)
+
+        const responseBody = await response.json()
+
+        expect(Array.isArray(responseBody)).toBe(true)
+        expect(responseBody.length).toBe(0)
+        expect(responseBody[0]).toBeUndefined()
+      })
+    })
   })
-
-  expect(response.status).toBe(201)
-
-  const responseBody = await response.json()
-
-  expect(Array.isArray(responseBody)).toBe(true)
-  expect(responseBody.length).toBeGreaterThan(0)
-  expect(responseBody[0]).not.toBeUndefined()
-
-  const response2 = await fetch('http://localhost:3000/api/v1/migrations', {
-    method: 'POST',
-  })
-
-  expect(response2.status).toBe(200)
-
-  const response2Body = await response2.json()
-
-  expect(Array.isArray(response2Body)).toBe(true)
-  expect(response2Body.length).toBe(0)
-  expect(response2Body[0]).toBeUndefined()
 })
