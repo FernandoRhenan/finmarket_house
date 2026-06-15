a7a5587cb8b6ccb43108b2c05f7916aa4a75d456
FernandoRhenan <fernandorhenan9@gmail.com>
2026-06-15 19:27:55 -0300

    feat: renew the user session pinging on `/api/v1/user`

---
+++ tests/integration/api/v1/user/get.test.js
@@ -0,0 +1,205 @@
+import { version as uuidVersion } from 'uuid'
+import orchestrator from 'tests/orchestrator.js'
+import { beforeAll, describe, expect, test, vitest } from 'vitest'
+import session from 'models/session'
+import setCookieParser from 'set-cookie-parser'
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices()
+  await orchestrator.cleanDatabase()
+  await orchestrator.runPendingMigrations()
+})
+
+describe('GET /api/v1/user', () => {
+  describe('Default user', () => {
+    test('With valid session', async () => {
+      const createdUser = await orchestrator.createUser({
+        username: 'UserWithValidSession',
+      })
+      const sessionObject = await orchestrator.createSession(createdUser.id)
+
+      const response = await fetch('http://localhost:3000/api/v1/user', {
+        headers: {
+          Cookie: `session_id=${sessionObject.token}`,
+        },
+      })
+
+      expect(response.status).toBe(200)
+
+      const cacheControl = response.headers.get('Cache-Control')
+      expect(cacheControl).toBe(
+        'no-store, no-cache, max-age=0, must-revalidate'
+      )
+
+      const responseBody = await response.json()
+
+      expect(responseBody).toEqual({
+        id: createdUser.id,
+        username: 'UserWithValidSession',
+        email: createdUser.email,
+        password: createdUser.password,
+        created_at: createdUser.created_at.toISOString(),
+        updated_at: createdUser.updated_at.toISOString(),
+      })
+
+      expect(uuidVersion(responseBody.id)).toBe(4)
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN()
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN()
+
+      // Session renewed assertions below
+      const renewedSessionObject = await session.findOneValidByToken(
+        sessionObject.token
+      )
+
+      expect(
+        renewedSessionObject.expires_at > sessionObject.expires_at
+      ).toBeTruthy()
+      expect(
+        renewedSessionObject.updated_at > sessionObject.updated_at
+      ).toBeTruthy()
+
+      // Set-Cookie assertions
+
+      const parsedSetCookie = setCookieParser(response, {
+        map: true,
+      })
+
+      expect(parsedSetCookie.session_id).toEqual({
+        name: 'session_id',
+        value: renewedSessionObject.token,
+        maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
+        path: '/',
+        httpOnly: true,
+      })
+    })
+
+    test('With valid session close to ending', async () => {
+      vitest.useFakeTimers({
+        now: new Date(Date.now() - session.EXPIRATION_IN_MILLISECONDS + 5000),
+      })
+
+      const createdUser = await orchestrator.createUser({
+        username: 'ValidSessionCloseToEnding',
+      })
+      const sessionObject = await orchestrator.createSession(createdUser.id)
+
+      vitest.useRealTimers()
+
+      const response = await fetch('http://localhost:3000/api/v1/user', {
+        headers: {
+          Cookie: `session_id=${sessionObject.token}`,
+        },
+      })
+
+      expect(response.status).toBe(200)
+
+      const responseBody = await response.json()
+
+      expect(responseBody).toEqual({
+        id: createdUser.id,
+        username: 'ValidSessionCloseToEnding',
+        email: createdUser.email,
+        password: createdUser.password,
+        created_at: createdUser.created_at.toISOString(),
+        updated_at: createdUser.updated_at.toISOString(),
+      })
+
+      expect(uuidVersion(responseBody.id)).toBe(4)
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN()
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN()
+
+      // Session renewed assertions below
+      const renewedSessionObject = await session.findOneValidByToken(
+        sessionObject.token
+      )
+
+      expect(
+        renewedSessionObject.expires_at > sessionObject.expires_at
+      ).toBeTruthy()
+      expect(
+        renewedSessionObject.updated_at > sessionObject.updated_at
+      ).toBeTruthy()
+
+      const dateNowByRenewedSession = new Date().setTime(
+        new Date(renewedSessionObject.expires_at) -
+          new Date(session.EXPIRATION_IN_MILLISECONDS)
+      )
+      const oldSessionExpiresAtDate = new Date().setTime(
+        sessionObject.expires_at
+      )
+      const diffBetweenOldAndRenewExpiresAt =
+        oldSessionExpiresAtDate - dateNowByRenewedSession
+
+      expect(diffBetweenOldAndRenewExpiresAt).greaterThan(4000)
+      expect(diffBetweenOldAndRenewExpiresAt).lessThan(5000)
+
+      // Set-Cookie assertions
+
+      const parsedSetCookie = setCookieParser(response, {
+        map: true,
+      })
+
+      expect(parsedSetCookie.session_id).toEqual({
+        name: 'session_id',
+        value: renewedSessionObject.token,
+        maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
+        path: '/',
+        httpOnly: true,
+      })
+    })
+
+    test('With nonexistent session', async () => {
+      const nonexistentToken =
+        '35157aceca195438761fb35c33369f29edfa28f38e7e7eb9acd63dfa1df4980cf2b141dbf17259386aa1156a1eb569c2'
+
+      const response = await fetch('http://localhost:3000/api/v1/user', {
+        headers: {
+          Cookie: `session_id=${nonexistentToken}`,
+        },
+      })
+
+      expect(response.status).toBe(401)
+
+      const responseBody = await response.json()
+
+      expect(responseBody).toEqual({
+        name: 'UnauthorizedError',
+        message: 'Usuário não possui sessão ativa.',
+        action: 'Verifique se este usuário está logado e tente novamente.',
+        status_code: 401,
+      })
+    })
+
+    test('With expired session', async () => {
+      vitest.useFakeTimers({
+        // - 10000 guarantees that that the session wil be expired
+        now: new Date(Date.now() - session.EXPIRATION_IN_MILLISECONDS - 10000),
+      })
+
+      const createdUser = await orchestrator.createUser({
+        username: 'UserWithExpiredSession',
+      })
+
+      const sessionObject = await orchestrator.createSession(createdUser.id)
+
+      vitest.useRealTimers()
+
+      const response = await fetch('http://localhost:3000/api/v1/user', {
+        headers: {
+          Cookie: `session_id=${sessionObject.token}`,
+        },
+      })
+
+      expect(response.status).toBe(401)
+
+      const responseBody = await response.json()
+
+      expect(responseBody).toEqual({
+        name: 'UnauthorizedError',
+        message: 'Usuário não possui sessão ativa.',
+        action: 'Verifique se este usuário está logado e tente novamente.',
+        status_code: 401,
+      })
+    })
+  })
+})
