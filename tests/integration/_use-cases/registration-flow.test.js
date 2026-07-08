9b019854d4952dead6e9143dde1c8353d6791811
FernandoRhenan <fernandorhenan9@gmail.com>
2026-07-08 10:38:48 -0300

    feat: add default feature `read:activation_token` when creating `user`

---
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -0,0 +1,50 @@
+import orchestrator from 'tests/orchestrator'
+import { beforeAll, describe, expect, test } from 'vitest'
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices()
+  await orchestrator.cleanDatabase()
+  await orchestrator.runPendingMigrations()
+  await orchestrator.deleteAllEmails()
+})
+
+describe('Use case: Registration flow (all successful)', () => {
+  test('Create user account', async () => {
+    const createUserResponse = await fetch(
+      'http://localhost:3000/api/v1/users',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: 'RegistrationFlow',
+          email: 'registration.flow@email.com',
+          password: 'registrationFlowPassword',
+        }),
+      }
+    )
+
+    expect(createUserResponse.status).toBe(201)
+
+    const createUserResponseBody = await createUserResponse.json()
+
+    expect(createUserResponseBody).toEqual({
+      id: createUserResponseBody.id,
+      username: 'RegistrationFlow',
+      email: 'registration.flow@email.com',
+      password: createUserResponseBody.password,
+      features: ['read:activation_token'],
+      created_at: createUserResponseBody.created_at,
+      updated_at: createUserResponseBody.updated_at,
+    })
+  })
+
+  test('Receive activation email', async () => {})
+
+  test('Active account', async () => {})
+
+  test('Login', async () => {})
+
+  test('Get user information', async () => {})
+})
--- tests/integration/api/v1/users/[username]/get.test.js
+++ tests/integration/api/v1/users/[username]/get.test.js
@@ -26,7 +26,7 @@ describe('GET /api/v1/users/[username]', () => {
         username: 'mesmoCase',
         email: user.email,
         password: responseBody.password,
-        features: [],
+        features: ['read:activation_token'],
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
       })
@@ -51,7 +51,7 @@ describe('GET /api/v1/users/[username]', () => {
         username: 'CaseDiferente',
         email: user.email,
         password: responseBody.password,
-        features: [],
+        features: ['read:activation_token'],
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
       })
