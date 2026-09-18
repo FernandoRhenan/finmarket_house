35ed280ebb53cd1193e61112a052b2a7d4aebb6e
FernandoRhenan <fernandorhenan9@gmail.com>
2026-09-18 07:52:57 -0300

    test: add `Default user` describe to test this user in `/tests/api/v1/status/get.test.js`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -25,6 +25,31 @@ describe('GET /api/v1/status', () => {
     })
   })
 
+  describe('Default user', () => {
+    test('Getting the system status', async () => {
+      const user = await orchestrator.createUser()
+      await orchestrator.activateUserById(user.id)
+      await orchestrator.addFeaturesToUser(user.id, ['read:status'])
+      const sessionObject = await orchestrator.createSession(user.id)
+
+      const response = await fetch(`${webserver.origin}/api/v1/status`, {
+        headers: {
+          cookie: `session_id=${sessionObject.token}`,
+        },
+      })
+      expect(response.status).toBe(200)
+
+      const responseBody = await response.json()
+
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString()
+
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt)
+      expect(responseBody.dependencies.database.database_version).toBe('')
+      expect(responseBody.dependencies.database.max_connections).toBe(100)
+      expect(responseBody.dependencies.database.current_connections).toBe(1)
+    })
+  })
+
   describe('Privileged user', () => {
     test('Getting the system status', async () => {
       const user = await orchestrator.createUser()
